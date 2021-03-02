9dab16bcec9d4fa18235c70660a36965a94d5362
KaransCo <asemaneabico@gmail.com>
2021-03-03 00:00:42 +0330

    initial commit

---
+++ SabaPayamak.Core/SabaPayamak.Core/Helper/Util.cs
@@ -0,0 +1,25 @@
+﻿using SabaPayamak.Core.Enum;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace SabaPayamak.Core.Helper
+{
+    public static class Util
+    {
+        public static string ToDescriptionString(this ResultStatus value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(
+                typeof(DescriptionAttribute),
+                false);
+
+            if (attributes != null &&
+                attributes.Length > 0)
+                return attributes[0].Description;
+            else
+                return value.ToString();
+        }
+    }
+}
---
+++ SabaPayamak.Core/SabaPayamak.Core/Models/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SabaPayamak.Core.Models
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string VirtualNumber { get; set; }
+        public int TokenValidDay { get; set; } = 1;
+    }
+}
